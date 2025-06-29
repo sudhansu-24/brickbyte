3f80ec9a9b227b96c723495489a476c640d46af8
Sudhansu <ssprusty98@gmail.com>
2025-06-30 00:34:02 +0530

    hardhatconfig for sepolia

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -16,12 +16,13 @@ const config: HardhatUserConfig = {
     }
   },
   networks: {
-    bscTestnet: {
+    sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111
     },
     hardhat: {
-      chainId: 97
+      chainId: 1337
     }
   },
   paths: {
